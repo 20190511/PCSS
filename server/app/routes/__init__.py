061a156a3c98f348c415c034503a894bb54796d6
yojun313 <moonyojun@gmail.com>
2026-01-18 00:49:43 +0900

    feat: add log routes to API router for improved logging functionality

--- server/app/routes/__init__.py
+++ server/app/routes/__init__.py
@@ -2,10 +2,11 @@ from fastapi import APIRouter
 from .conf_routes import router as conf_router
 from .search_routes import router as search_router
 from .page_routes import router as page_router
-from .author_routes import router as author_router
+from .log_routes import router as log_router
 
 api_router = APIRouter()    
 
 api_router.include_router(conf_router, prefix="/conf", tags=["conferences"])
 api_router.include_router(search_router, prefix="/search", tags=["search"])
 api_router.include_router(page_router, prefix="/page", tags=["page"])
+api_router.include_router(log_router, prefix="/logs", tags=["logs"])
